a5e4693af2cee0a9e1382a418da3eddbbddd8d6e
rikvdlooi <rikkavdl@gmail.com>
2017-01-07 18:06:06 +0100

    Added 127.0.0.1 to meteor start script to prevent ports being opened on the firewall

--- src/modules/meteor/assets/templates/start.sh
+++ src/modules/meteor/assets/templates/start.sh
@@ -20,7 +20,7 @@ set -e
 docker run \
   -d \
   --restart=always \
-  --publish=$PORT:80 \
+  --publish=127.0.0.1:$PORT:80 \
   --volume=$BUNDLE_PATH:/bundle \
   --hostname="$HOSTNAME-$APPNAME" \
   --env-file=$ENV_FILE \
